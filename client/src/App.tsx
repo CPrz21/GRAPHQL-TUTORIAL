bc960576f374720f404de7449c4d502a8e6823a1
CPrz21 <faselona@gmail.com>
2022-10-08 12:25:14 -0600

    add v1 interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
 import "./App.css";
+
+import client from "./client";
 import { DisplayData } from "./components";
 
 function App() {
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: "http://localhost:4000/graphql",
-  });
   return (
     <ApolloProvider client={client}>
       <div className="App">
--- client/src/components/display-data/display-data.tsx
+++ client/src/components/display-data/display-data.tsx
@@ -1,15 +1,26 @@
-import { useQuery, useLazyQuery } from "@apollo/client";
 import { useState } from "react";
+import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
 
-import { GET_ALL_USERS, GET_ALL_MOVIES, GET_MOVIE } from "../../graphql";
+import {
+  GET_ALL_USERS,
+  GET_ALL_MOVIES,
+  GET_MOVIE,
+  CREATE_USER_MUTATION,
+} from "../../graphql";
 import { Movies, MoviesData, UsersData } from "../../interfaces";
 
 export const DisplayData = () => {
   const [movieSearched, setMovieSearched] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  const [nationality, setNationality] = useState("");
+
   const {
     loading: usersLoading,
     error: usersError,
     data: usersData,
+    refetch: refetchUsers,
   } = useQuery<UsersData>(GET_ALL_USERS);
 
   const { data: moviesData } = useQuery<MoviesData>(GET_ALL_MOVIES);
@@ -17,6 +28,8 @@ export const DisplayData = () => {
   const [fetchMovie, { data: movieData, error: movieError }] = useLazyQuery<{
     movie: Omit<Movies, "isInTheaters">;
   }>(GET_MOVIE);
+
+  const [createUser] = useMutation(CREATE_USER_MUTATION);
 
   if (usersLoading) {
     return <p>Loading Your Data</p>;
@@ -28,8 +41,45 @@ export const DisplayData = () => {
 
   return (
     <div>
-      {/* {usersData &&
-        usersData.users.map(({name, username, nationality, id}) => {
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="age"
+        onChange={(e) => setAge(Number(e.target.value))}
+      />
+      <input
+        type="text"
+        placeholder="nationality"
+        onChange={(e) => setNationality(e.target.value.toUpperCase())}
+      />
+      <button
+        onClick={() => {
+          createUser({
+            variables: {
+              input: {
+                name,
+                username,
+                age,
+                nationality,
+              },
+            },
+          });
+          refetchUsers();
+        }}
+      >
+        Create user
+      </button>
+      {usersData &&
+        usersData.users.map(({ name, username, nationality, id }) => {
           return (
             <div key={id}>
               <h3>Name: {name}</h3>
@@ -40,7 +90,7 @@ export const DisplayData = () => {
               <hr />
             </div>
           );
-        })} */}
+        })}
 
       {moviesData &&
         moviesData.movies.map(
